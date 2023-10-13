d8e6570dd42cea6b7e39ae5ca2f1bf29680a59fb
inwick <indikasandun2015@gmail.com>
2023-10-13 20:09:32 +0530

    Login and Register UI Enhancement

--- src/components/Nav/primary-nav-bar/PrimaryNavBar.jsx
+++ src/components/Nav/primary-nav-bar/PrimaryNavBar.jsx
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from "react";
-import "./PrimaryNavBar.scss";
-import logo from "../../../assets/logo.png";
-import { useLocation, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import logo from '../../../assets/logo.png';
+import './PrimaryNavBar.scss';
 
 const PrimaryNavBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [role, setRole] = useState("");
-  const [active, setActive] = useState("");
+  const [role, setRole] = useState('');
+  const [active, setActive] = useState('');
 
   useEffect(() => {
-    const role = localStorage.getItem("role");
-    if (role === null || role === undefined || role === "") {
-      navigate("/");
+    const role = localStorage.getItem('role');
+    if (role === null || role === undefined || role === '') {
+      navigate('/');
     } else {
       setRole(role);
     }
   }, []);
 
   useEffect(() => {
-    if (location.pathname === "/dashboard/traveller") {
-      setActive("travellers");
-    } else if (location.pathname === "/dashboard/booking") {
-      setActive("bookings");
-    } else if (location.pathname === "/dashboard/scheduling") {
-      setActive("schedules");
+    if (location.pathname === '/dashboard/traveller') {
+      setActive('travellers');
+    } else if (location.pathname === '/dashboard/booking') {
+      setActive('bookings');
+    } else if (location.pathname === '/dashboard/scheduling') {
+      setActive('schedules');
     }
   }, [location.pathname]);
 
@@ -35,67 +35,61 @@ const PrimaryNavBar = () => {
     } catch (e) {
       console.error(e);
     } finally {
-      navigate("/");
+      navigate('/');
     }
   };
 
   return (
-    <div
-      className={`vh-100 d-flex flex-column justify-content-between py-4 primaryNavBar`}
-    >
+    <div className={`vh-100 d-flex flex-column justify-content-between py-4 primaryNavBar`}>
       <div>
         <div>
-          <a href="/home">
+          <a href='/home'>
             <img
               src={logo}
-              alt="Logo"
+              alt='Logo'
               className={`d-block mx-auto mb-3 mt-2 logoImg cursor-pointer`}
             />
           </a>
         </div>
-        <div className="pt-5 ps-4">
+        <div className='pt-5 ps-4'>
           <h2
-            className={`pb-1 subNavLink ${
-              active == "travellers" ? "activeLink" : "deactiveLink"
-            }`}
+            className={`pb-1 subNavLink ${active == 'travellers' ? 'activeLink' : 'deactiveLink'}`}
             onClick={() => {
-              navigate("/dashboard/traveller");
+              navigate('/dashboard/traveller');
             }}
           >
-            Travellers
+            Travelers
           </h2>
-          <hr className="linkDivider" />
+          <hr className='linkDivider' />
           <h2
-            className={`pb-1 subNavLink ${
-              active == "bookings" ? "activeLink" : "deactiveLink"
-            }`}
+            className={`pb-1 subNavLink ${active == 'bookings' ? 'activeLink' : 'deactiveLink'}`}
             onClick={() => {
-              navigate("/dashboard/booking");
+              navigate('/dashboard/booking');
             }}
           >
             Bookings
           </h2>
 
-          {role == "officer" ? (
+          {role == 'officer' ? (
             <>
-              <hr className="linkDivider" />
+              <hr className='linkDivider' />
               <h2
                 className={`pb-1 subNavLink ${
-                  active == "schedules" ? "activeLink" : "deactiveLink"
+                  active == 'schedules' ? 'activeLink' : 'deactiveLink'
                 }`}
                 onClick={() => {
-                  navigate("/dashboard/scheduling");
+                  navigate('/dashboard/scheduling');
                 }}
               >
                 Schedules
               </h2>
             </>
           ) : (
-            ""
+            ''
           )}
         </div>
-        <div className=" ps-4 logoutSection d-flex flex-column justify-content-end">
-          <p className="logoutBtn" onClick={handleLogout}>
+        <div className=' ps-4 logoutSection d-flex flex-column justify-content-end'>
+          <p className='logoutBtn' onClick={handleLogout}>
             Logout
           </p>
         </div>
